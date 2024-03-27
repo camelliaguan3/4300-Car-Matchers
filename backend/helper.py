babdd70d9f3ed3c1fd52d367b4d3f3185ffb16df
Camellia Guan <camelliaguan3@gmail.com>
2024-03-27 14:20:28 -0400

    Fixed something?

--- backend/helper.py
+++ backend/helper.py
@@ -102,11 +102,11 @@ def build_inverted_index_final(cars):
             if color != 'Tbd':
                 colors.append(color.lower())
         
-        for color in car['color options']:
-            colors += color.lower().split()
+        # for color in car['color options']:
+        #     colors += color.lower().split()
 
         original = (car['make'].lower().replace('-', ' ').split() 
-                    + car['model'].lower().replace('-', ' ').split() 
+                    + car['model'].lower().replace('-', ' ').split()
                     + colors
                     + car['converted car type'].lower().split()
                     + car['car type (epa classification)'].lower().split())
